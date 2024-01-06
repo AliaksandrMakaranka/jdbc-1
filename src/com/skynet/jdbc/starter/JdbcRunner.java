2e2a2431223151868adee80d5ddf4796b91d8b3f
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2024-01-06 08:33:09 +0100

    feat: 03.connect to database

--- src/com/skynet/jdbc/starter/JdbcRunner.java
+++ src/com/skynet/jdbc/starter/JdbcRunner.java
@@ -1,7 +1,18 @@
 package com.skynet.jdbc.starter;
 
-public class JdbcRunner {
-    public static void main(String[] args) {
 
+import com.skynet.jdbc.starter.util.ConnectionManager;
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JdbcRunner {
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+
+        try (Connection connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
 }
